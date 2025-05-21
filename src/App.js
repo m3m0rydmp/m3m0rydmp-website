9d1435f5992efa7e23718e18089a08bc6a0ee282
m3m0rydmp <robwilssacote@gmail.com>
2025-05-21 17:35:00 +0800

    Changed Designs, added pages

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Switch, useLocation } from "react-router-dom";
 import $ from "jquery";
 import "./App.scss";
 import Header from "./components/Header";
@@ -7,20 +8,136 @@ import About from "./components/About";
 import Experience from "./components/Experience";
 import Projects from "./components/Projects";
 import Skills from "./components/Skills";
+import BlogPage from "./components/blog/test_blog"; // Dynamic blog page
+import BlogListPage from "./components/blog/BlogListPage";
+import BlogDetailPage from "./components/blog/BlogDetailPage";
+import BlogLayout from "./components/blog/BlogLayout";
+import ThemeBackground from "./components/ThemeBackground";
+import { Live2DProvider } from './components/blog/Live2DContext';
+
+// Main site content as a component
+const MainSite = ({ 
+  sharedData, 
+  resumeData, 
+  onLanguageChange 
+}) => (
+  <div>
+    <Header sharedData={sharedData.basic_info} />
+    <div className="col-md-12 mx-auto text-center language">
+      <div
+        onClick={() =>
+          onLanguageChange(
+            window.$primaryLanguage,
+            window.$secondaryLanguageIconId
+          )
+        }
+        style={{ display: "inline" }}
+      >
+        <span
+          className="iconify language-icon mr-5"
+          data-icon="twemoji-flag-for-flag-united-kingdom"
+          data-inline="false"
+          id={window.$primaryLanguageIconId}
+        ></span>
+      </div>
+      <div
+        onClick={() =>
+          onLanguageChange(
+            window.$secondaryLanguage,
+            window.$primaryLanguageIconId
+          )
+        }
+        style={{ display: "inline" }}
+      >
+        <span
+          className="iconify language-icon"
+          data-icon="twemoji-flag-for-flag-philippines"
+          data-inline="false"
+          id={window.$secondaryLanguageIconId}
+        ></span>
+      </div>
+    </div>
+    <About
+      resumeBasicInfo={resumeData.basic_info}
+      sharedBasicInfo={sharedData.basic_info}
+    />
+    <Projects
+      resumeProjects={resumeData.projects}
+      resumeBasicInfo={resumeData.basic_info}
+    />
+    <Skills
+      sharedSkills={sharedData.skills}
+      resumeBasicInfo={resumeData.basic_info}
+    />
+    <Experience
+      resumeExperience={resumeData.experience}
+      resumeBasicInfo={resumeData.basic_info}
+    />
+    <Footer sharedBasicInfo={sharedData.basic_info} />
+  </div>
+);
+
+// Create a wrapper component to handle location changes
+const AppContent = ({ onLocationChange, sharedData, resumeData, onLanguageChange }) => {
+  const location = useLocation();
+  
+  React.useEffect(() => {
+    onLocationChange(location.pathname);
+  }, [location.pathname, onLocationChange]);
+
+  return (
+    <Switch>
+      <Route path="/blog" render={({ match: { path } }) => (
+        <BlogLayout>
+          <Switch>
+            <Route exact path="/blog/:project/:blogId" component={BlogDetailPage} />
+            <Route exact path="/blog/:project" component={BlogListPage} />
+          </Switch>
+        </BlogLayout>
+      )} />
+      <Route path="*" render={() => (
+        <MainSite 
+          sharedData={sharedData} 
+          resumeData={resumeData}
+          onLanguageChange={onLanguageChange}
+        />
+      )} />
+    </Switch>
+  );
+};
 
 class App extends Component {
-
   constructor(props) {
     super();
     this.state = {
       foo: "bar",
       resumeData: {},
       sharedData: {},
+      currentPath: '/'
     };
   }
 
+  handleLocationChange = (pathname) => {
+    this.setState({ currentPath: pathname }, () => {
+      // Reset the page state when navigating
+      if (pathname === '/') {
+        this.initializeMainSite();
+      }
+    });
+  };
+
+  initializeMainSite = () => {
+    this.loadSharedData();
+    this.applyPickedLanguage(
+      window.$primaryLanguage,
+      window.$secondaryLanguageIconId
+    );
+  };
+
   applyPickedLanguage(pickedLanguage, oppositeLangIconId) {
-    this.swapCurrentlyActiveLanguage(oppositeLangIconId);
+    if (this.state.currentPath === '/') {
+      this.swapCurrentlyActiveLanguage(oppositeLangIconId);
+    }
     document.documentElement.lang = pickedLanguage;
     var resumePath =
       document.documentElement.lang === window.$primaryLanguage
@@ -34,20 +151,26 @@ class App extends Component {
       oppositeLangIconId === window.$primaryLanguageIconId
         ? window.$secondaryLanguageIconId
         : window.$primaryLanguageIconId;
-    document
-      .getElementById(oppositeLangIconId)
-      .removeAttribute("filter", "brightness(40%)");
-    document
-      .getElementById(pickedLangIconId)
-      .setAttribute("filter", "brightness(40%)");
+    
+    const oppositeIcon = document.getElementById(oppositeLangIconId);
+    const pickedIcon = document.getElementById(pickedLangIconId);
+    
+    if (oppositeIcon) {
+      oppositeIcon.removeAttribute("filter", "brightness(40%)");
+    }
+    if (pickedIcon) {
+      pickedIcon.setAttribute("filter", "brightness(40%)");
+    }
   }
 
   componentDidMount() {
-    this.loadSharedData();
-    this.applyPickedLanguage(
-      window.$primaryLanguage,
-      window.$secondaryLanguageIconId
-    );
+    // Initialize based on current path
+    if (this.state.currentPath === '/') {
+      this.initializeMainSite();
+    } else {
+      document.documentElement.lang = window.$primaryLanguage;
+      this.loadResumeFromPath('res_primaryLanguage.json');
+    }
   }
 
   loadResumeFromPath(path) {
@@ -59,7 +182,7 @@ class App extends Component {
         this.setState({ resumeData: data });
       }.bind(this),
       error: function (xhr, status, err) {
-        alert(err);
+        console.error("Error loading resume data:", err);
       },
     });
   }
@@ -74,67 +197,35 @@ class App extends Component {
         document.title = `${this.state.sharedData.basic_info.name}`;
       }.bind(this),
       error: function (xhr, status, err) {
-        alert(err);
+        console.error("Error loading shared data:", err);
       },
     });
   }
 
   render() {
     return (
-      <div>
-        <Header sharedData={this.state.sharedData.basic_info} />
-        <div className="col-md-12 mx-auto text-center language">
-          <div
-            onClick={() =>
-              this.applyPickedLanguage(
-                window.$primaryLanguage,
-                window.$secondaryLanguageIconId
-              )
-            }
-            style={{ display: "inline" }}
-          >
-            <span
-              className="iconify language-icon mr-5"
-              data-icon="twemoji-flag-for-flag-united-kingdom"
-              data-inline="false"
-              id={window.$primaryLanguageIconId}
-            ></span>
-          </div>
-          <div
-            onClick={() =>
-              this.applyPickedLanguage(
-                window.$secondaryLanguage,
-                window.$primaryLanguageIconId
-              )
-            }
-            style={{ display: "inline" }}
-          >
-            <span
-              className="iconify language-icon"
-              data-icon="twemoji-flag-for-flag-philippines"
-              data-inline="false"
-              id={window.$secondaryLanguageIconId}
-            ></span>
-          </div>
+      <>
+        <ThemeBackground />
+        <div className="app-content">
+          <Router>
+            <Live2DProvider>
+              <AppContent 
+                onLocationChange={this.handleLocationChange}
+                sharedData={this.state.sharedData}
+                resumeData={this.state.resumeData}
+                onLanguageChange={this.applyPickedLanguage}
+              />
+            </Live2DProvider>
+          </Router>
         </div>
-        <About
-          resumeBasicInfo={this.state.resumeData.basic_info}
-          sharedBasicInfo={this.state.sharedData.basic_info}
-        />
-        <Projects
-          resumeProjects={this.state.resumeData.projects}
-          resumeBasicInfo={this.state.resumeData.basic_info}
-        />
-        <Skills
-          sharedSkills={this.state.sharedData.skills}
-          resumeBasicInfo={this.state.resumeData.basic_info}
-        />
-        <Experience
-          resumeExperience={this.state.resumeData.experience}
-          resumeBasicInfo={this.state.resumeData.basic_info}
-        />
-        <Footer sharedBasicInfo={this.state.sharedData.basic_info} />
-      </div>
+        <style>{`
+          .app-content {
+            position: relative;
+            z-index: 1;
+            min-height: 100vh;
+          }
+        `}</style>
+      </>
     );
   }
 }
